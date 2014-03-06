86e074414728ca0d95e38472681a04b8fe11ccd1
Matt Ramirez <mbramire@live.com>
2014-03-06 16:33:57 -0500

    moved helpers

--- app.rb
+++ app.rb
@@ -17,33 +17,6 @@ class Comment < ActiveRecord::Base
   belongs_to :post
 end
 
-helpers do
-  # If @title is assigned, add it to the page's title.
-  def title
-    if @title
-      "#{@title} -- My Blog"
-    else
-      "My Blog"
-    end
-  end
- 
-  # Format the Ruby Time object returned from a post's created_at method
-  # into a string that looks like this: 06 Jan 2012
-  def pretty_date(time)
-   time.strftime("%B %d, %Y")
-  end
 
-  def post_show_page?
-    request.path_info =~ /\/posts\/\d+$/
-  end
-
-  def delete_post_button(post_id)
-    haml :_delete_post_button, locals: { post_id: post_id}
-  end
-
-  def truncate(body)
-    "#{body.slice!(0..20)}..."
-  end
-end
-
+require_relative 'helpers/helpers'
 require_relative 'routes/routes'
